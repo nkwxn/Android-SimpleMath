b4a77e04bced8b86617e773f561c4fe646f8855c
nkwxn <nkwxn@users.noreply.github.com>
2020-04-25 10:58:48 +0700

    x

--- app/src/main/java/personal/simplemath/MainActivity.java
+++ app/src/main/java/personal/simplemath/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+//    command untuk penghitungan
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
